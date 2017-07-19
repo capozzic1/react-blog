c49eec04a37fb7e454539c0b2656ad17f93029e2
Chris <capozzic1@gmail.com>
2017-07-19 20:12:06 +0900

    Update

---
+++ components/Navigation/Navigation.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+export default class Navigation extends React.Component {
+render(){
+  return (
+    <nav>
+      <a href="#">Lucid Web Dream</a>
+      <a href="#">About</a>
+      <a href="#">Contact</a>
+      //<a href="#">Portfolio</a>
+    </nav>
+  );
+  }
+}
---
+++ components/Navigation/navigation.spec.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import {expect} from 'chai';
+
+import Navigation from './Navigation.jsx';
+
+
+describe('<Navigation />', function(){
+  //it should have 4 links, lucidwebdream, about, contact, portfolio
+  it('should have 4 links', function(){
+    const wrapper = shallow(<Navigation />);
+    expect(wrapper.find('a')).to.have.length(4);
+  });
+
+});
