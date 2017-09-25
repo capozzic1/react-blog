477b697b3ae8fbd64ca954bf58d33c6424df4e8a
Chris <capozzic1@gmail.com>
2017-09-25 20:50:57 +0900

    Added in pm2

--- server/config/main.js
+++ server/config/main.js
@@ -2,7 +2,7 @@ module.exports = {
   // Secret key for JWT signing and encryption
   secret: 'super secret passphrase',
   // Database connection information
-  database: process.env.MONGODB_URI,
+  database: 'mongodb://127.0.0.1:27017',
 
 };
 
