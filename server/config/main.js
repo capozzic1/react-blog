b20ef0c229f9eb740cd060062c39d92a4bd9a15f
Chris <capozzic1@gmail.com>
2017-09-25 20:52:02 +0900

    change db uri

--- server/config/main.js
+++ server/config/main.js
@@ -2,7 +2,7 @@ module.exports = {
   // Secret key for JWT signing and encryption
   secret: 'super secret passphrase',
   // Database connection information
-  database: 'mongodb://127.0.0.1:27017',
+  database: process.env.MONGODB_URI,
 
 };
 
